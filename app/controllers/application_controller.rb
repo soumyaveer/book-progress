6a9cd64ea7fe2bc6518dfa6f08709e6cd2c9aebe
Soumya Veer <veer.soumya@gmail.com>
2017-09-07 15:39:17 -0700

    add helpers to application_controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,4 +7,10 @@ class ApplicationController < Sinatra::Base
     enable :sessions
     set :session_secret, "secret"
   end
+
+  helpers do
+    def current_user
+      User.find(session[:user_id])
+    end
+  end
 end
