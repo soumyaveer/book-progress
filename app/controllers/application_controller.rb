15f2a239820ba3219aeeccfa77135ee0e364cc44
Soumya Veer <veer.soumya@gmail.com>
2017-09-07 16:13:11 -0700

    add public/stylesheet folder

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,6 +7,9 @@ class ApplicationController < Sinatra::Base
     enable :sessions
     set :session_secret, "secret"
   end
+  get '/' do
+    erb :'/index'
+  end
 
   helpers do
     def current_user
