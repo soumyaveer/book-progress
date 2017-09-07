dd20706a50046181d593cc8253296c7274b013f4
Soumya Veer <veer.soumya@gmail.com>
2017-09-07 13:31:55 -0700

    add tests for has many validation for BookProgression

--- spec/models/book_progression_spec.rb
+++ spec/models/book_progression_spec.rb
@@ -1,17 +1,35 @@
 describe BookProgression do
   describe 'validations' do
     before do
-      @user = User.new(username: "Harry Potter",
+      @user1 = User.create(username: "Harry Potter",
                        email: "harry_potter@hogwarts.edu",
                        password: "harry1")
 
-      @book = Book.new(title: "Fantastic Beasts and Where to Find Them.",
+      @user2 = User.create(username: "Hermoine Granger",
+                          email: "hermoine_granger@hogwarts.edu",
+                          password: "smartwitch@1")
+
+      @book1 = Book.create(title: "Fantastic Beasts and Where to Find Them.",
                        author: "Newt Scamander",
                        pages: 500)
 
-      @book_progression = BookProgression.new(user_id: @user.id, book_id: @book.id, current_page: 100)
+      @book2 = Book.create(title: "A History of Magic",
+                          author: "Bathilda Bagshot",
+                          pages: 1000)
+
+      @book_progression1 = BookProgression.create(user_id: @user1.id, book_id: @book1.id, current_page: 100)
+      @book_progression1 = BookProgression.create(user_id: @user2.id, book_id: @book1.id, current_page: 100)
+      @book_progression1 = BookProgression.create(user_id: @user2.id, book_id: @book2.id, current_page: 100)
+
+
     end
 
+    it 'returns users reading the book' do
+      expect(@book.users).to match_array([@user1 ,@user2])
+    end
 
+    it 'returns books read by user' do
+      expect(@user2.books).to match_array([@book1, @book2])
+    end
   end
 end
