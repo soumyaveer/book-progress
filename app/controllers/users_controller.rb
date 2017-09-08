ddafbf22d2996cd9c1d6cf35ed0d19e4709e62a1
Soumya Veer <veer.soumya@gmail.com>
2017-09-07 18:31:07 -0700

    add login form and login action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -16,4 +16,22 @@ class UsersController < ApplicationController
       redirect '/signup'
     end
   end
+
+  get '/login' do
+    if logged_in?
+      redirect '/homepage'
+    else
+      erb :'/users/login'
+    end
+  end
+
+  post '/login' do
+    user = User.find_by(username: params[:username])
+    if user && user.authenticate(params[:password])
+      session[:user_id] = user.id
+      redirect '/homepage'
+    else
+      redirect '/login'
+    end
+  end
 end
