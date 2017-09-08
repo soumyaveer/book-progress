8d623216e6225edef0169576d7fb43706f18f332
Soumya Veer <veer.soumya@gmail.com>
2017-09-08 16:04:23 -0700

    add update action to books_controller

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -29,6 +29,32 @@ class BooksController < ApplicationController
     end
   end
 
+  # update
+  get '/books/:id/edit' do
+    if logged_in?
+      @book = Book.find(params[:id])
+      @progress = BookProgression.find_by(book_id: params[:id])
+      erb :'/books/edit'
+    else
+      redirect '/login'
+    end
+  end
+
+  patch '/books/:id' do
+    @book = Book.find(params[:id])
+    @book.title = params[:title]
+    @book.author = params[:author]
+    @book.pages = params[:pages].to_i
+
+    if @book.save
+      @progress = BookProgression.find_by(book_id: params[:id])
+      @progress.current_page = params[:current_page].to_i
+      redirect "/books/#{@book.id}"
+    else
+      redirect "/books/#{@books.id}/edit"
+    end
+  end
+  
   # show
   get '/books/:id' do
     if logged_in?
