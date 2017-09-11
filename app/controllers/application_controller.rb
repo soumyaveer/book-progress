35942334df6766fa5856d9fb1eba5d748cbcd0ba
Soumya Veer <veer.soumya@gmail.com>
2017-09-11 16:11:32 -0700

    refactor root in ApplicationController

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,7 +9,11 @@ class ApplicationController < Sinatra::Base
   end
 
   get '/' do
-    erb :'/index'
+    if logged_in?
+      redirect '/users/homepage'
+    else
+      erb :'/index'
+    end
   end
 
   helpers do
