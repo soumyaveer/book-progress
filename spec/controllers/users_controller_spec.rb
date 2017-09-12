023efbf800fea76a89b1a4167c3a650f0e84e60a
Soumya Veer <veer.soumya@gmail.com>
2017-09-12 16:31:14 -0700

    complete UserController#Signup tests

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -31,5 +31,53 @@ describe UsersController do
       post '/signup', params
       expect(last_response.location).to include("/signup")
     end
+
+    it 'redirects user to signup page if email is not present' do
+      params = {
+        username: "test-name",
+        email: "",
+        password: "test1"
+      }
+
+      post '/signup', params
+      expect(last_response.location).to include("/signup")
+    end
+
+    it 'redirects user to signup page if email is not in the right format' do
+      params = {
+        username: "test-name",
+        email: "email@",
+        password: "test1"
+      }
+
+      post '/signup', params
+      expect(last_response.location).to include("/signup")
+    end
+
+
+    it 'redirects user to signup page if password is not present' do
+      params = {
+        username: "test-name",
+        email: "",
+        password: "test1"
+      }
+
+      post '/signup', params
+      expect(last_response.location).to include("/signup")
+    end
+
+    it 'redirects to users homepage if the user is already signed in' do
+      user = User.create(:username => "test-name", :email => "email@test.com", :password => "test1")
+      params = {
+        :username => "test-name",
+        :email => "email@test.com",
+        :password => "test1"
+      }
+      post '/signup', params
+      session = {}
+      session[:user_id] = user.id
+      get '/signup'
+      expect(last_response.location).to include('/homepage')
+    end
   end
 end
