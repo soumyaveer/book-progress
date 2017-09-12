b8f894aea8f7cd1995a88712f0cde3afc4f4b1f5
Soumya Veer <veer.soumya@gmail.com>
2017-09-11 17:08:06 -0700

    add slug for users and add spec.md to book-progress

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -53,8 +53,8 @@ class UsersController < ApplicationController
     end
   end
 
-  get '/users/:id' do
-    @user = User.find(params[:id])
+  get '/users/:slug' do
+    @user = User.find_by_slug(params[:slug])
     erb :'/users/show'
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -9,4 +9,11 @@ class User < ActiveRecord::Base
             format: {with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i }
   validates :password, presence: true
 
+  def slug
+    self.username.downcase.split(" ").join("-")
+  end
+
+  def self.find_by_slug(slug)
+    User.all.find {|user| user.slug == slug}
+  end
 end
