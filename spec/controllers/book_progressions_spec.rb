d38919dbdcb8da45b20b5fbcff9e647f6727459f
Soumya Veer <veer.soumya@gmail.com>
2017-09-12 18:00:01 -0700

    Add tests for BookProgressionsController#new action

--- spec/controllers/book_progressions_spec.rb
+++ spec/controllers/book_progressions_spec.rb
@@ -66,4 +66,73 @@ describe BookProgressionsController do
       end
     end
   end
+
+  describe "new action" do
+    context 'logged in' do
+      before do
+        @user1 = User.create(:username => "test-name1", :email => "email1@test.com", :password => "test1")
+        @user2 = User.create(:username => "test-name2", :email => "email2@test.com", :password => "test2")
+
+        @book1 = Book.create(title: "book-name1", author: "book-author", pages: 300)
+        visit '/login'
+
+        fill_in(:username, with: "test-name1")
+        fill_in(:password, with: "test1")
+
+        click_button 'Log In'
+      end
+
+      it 'redirects to create new book page' do
+        visit '/book_progressions/new'
+        expect(page.status_code).to eql(200)
+      end
+
+      it 'allows the creation of new progress' do
+        visit '/book_progressions/new'
+        fill_in(:title, with: "New Title")
+        fill_in(:author, with: "some author")
+        fill_in(:pages, with: 300)
+        fill_in(:current_page, with: 100)
+
+        click_button 'Submit'
+
+        user = User.find_by(id: @user1.id)
+        book = Book.find_by(id: @book1.id)
+
+        expect(user).to be_instance_of(User)
+        expect(book).to be_instance_of(Book)
+        expect(page.status_code).to eql(200)
+      end
+
+      it 'does not let user create a progress in another user\'s account' do
+        visit '/book_progressions/new'
+        fill_in(:title, with: "New Title")
+        fill_in(:author, with: "some author")
+        fill_in(:pages, with: 300)
+        fill_in(:current_page, with: 100)
+
+        click_button 'Submit'
+        logged_in_user = User.find_by(id: @user1.id)
+        not_logged_in_user = User.find_by(id: @user2.id)
+        book2 = Book.find_by(title: "New Title")
+        book_progression = BookProgression.find_by(book_id: book2.id)
+
+        expect(book_progression).to be_instance_of(BookProgression)
+        expect(book_progression.user_id).to eq(logged_in_user.id)
+        expect(book_progression.user_id).not_to eql(not_logged_in_user.id)
+      end
+
+      it 'does not let user create a blank progression' do
+        visit '/book_progressions/new'
+        fill_in(:title, with: "")
+        fill_in(:author, with: "some author")
+        fill_in(:pages, with: 300)
+        fill_in(:current_page, with: 100)
+        click_button 'Submit'
+
+        expect(Book.find_by(title: "")).to eql(nil)
+        expect(page.current_path).to eql("/book_progressions/new")
+      end
+    end
+  end
 end
